eadee345f907e0d6044dc2fa675cb9b685f00c57
James Gong <james.gong@accesshq.com>
2016-09-15 22:22:07 +1000

    Minor refactors

--- r_files/runner.r
+++ r_files/runner.r
@@ -3,11 +3,19 @@ repos = "http://cran.rstudio.com/"
 list_of_packages = c("utils","foreign","pastecs","mlogit","graphics","VGAM","ZeligChoice","aod","plotrix")
 
 new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-print(new_packages)
-
-if(length(new.packages)) {
+if(length(new_packages) > 0) {
 	install.packages(new_packages, repos="http://cran.rstudio.com/")
 }
+
+library("utils")   # to use csv read function
+library("foreign") # to use spss read function
+library("pastecs") # to use descriptive statistics function
+library("mlogit")
+library("graphics")
+library("VGAM")
+library("ZeligChoice") # This package is for actual mlogit function
+library("aod")
+library("plotrix")
 
 rm(list = ls())
 print("-----> Finished Loading Packages <-----")
