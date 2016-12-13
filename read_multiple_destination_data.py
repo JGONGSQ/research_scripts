2893ea341a04e552bfc958b781408913488b75ce
James Gong <james.gong@accesshq.com>
2016-12-13 14:06:53 +1100

    Minor edit on data generator

--- read_multiple_destination_data.py
+++ read_multiple_destination_data.py
@@ -5,8 +5,10 @@ from py_files.data import trim_data
 from py_files.settings import *
 
 # utility_variables = ['HOMESLA', 'ORIGIN', 'HOUSEHOLD', 'UNDER15', 'OVER15', 'PARENT', 'YOUNGEST', 'AGEGROUP', 'CH15TO24', 'GENDER', 'MARITAL', 'EMPLOYMENT', 'HOUSINC', 'LIFECYCLE']
+# droped_variables = ['PARENT', 'YOUNGEST', 'CH15TO24']
+variable_need_to_be_analysed = ['AGEGROUP']
 
-utility_variables = ['ORIGIN', 'HOMESLA']
+utility_variables = ['ORIGIN', 'HOMESLA', 'HOUSEHOLD', 'UNDER15', 'OVER15', 'GENDER', 'MARITAL', 'EMPLOYMENT', 'HOUSINC', 'LIFECYCLE']
 
 results = trim_data(
     input_file=TEST_INPUT_DATA_PATH,
