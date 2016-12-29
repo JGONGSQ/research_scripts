ee96ed7869d2755fe46393d6f90ba300503029f2
James <james.gong927@gmail.com>
2016-12-29 20:15:01 +0800

    Minor fix

--- execute_results.py
+++ execute_results.py
@@ -3,12 +3,6 @@
 from py_files.settings import *
 from py_files.data import *
 
-
-test_file = 'results_4_HOMEREGN.txt'
-
+# filter the file to see if converge or not
 filter_files(RESULTS_PATH)
-# value = is_file_converge(RESULTS_PATH + '/' + test_file)
-# print('Value:', value)
-# print(ALL_VARIABLES)
-# print(EXECLUDE_VARIABLE_4)
-# print(list(set(ALL_VARIABLES).difference(set(EXECLUDE_VARIABLE_1))))
+
