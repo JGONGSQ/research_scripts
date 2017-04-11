8418b154671ad798652dd055b8eef555abf89f06
James <james.gong927@gmail.com>
2017-04-11 10:56:05 +1000

    Add conditional logit model into the file

---
+++ ConditionalLogit/conditional_logit.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+from collections import OrderedDict
+
+import pandas as pd
+import numpy as np
+
+import pylogit as pl
+
+
+long_testing_data = pd.read_csv("/Users/daddyspro/Desktop/conditional_logit_test_data.csv")
+print(long_testing_data)
+# print(long_testing_data.columns)
+
+
+# general settings
+custom_alt_id = "mode_id"
+
+obs_id_column = "choice_situation"
+
+choice_column = "choice"
+
+
+basic_specification = OrderedDict()
+basic_names = OrderedDict()
+
+basic_specification["intercept"] = [1, 2]
+basic_names["intercept"] = ['ASC_NSW',
+                            'ASC_VIC'
+                            ]
+
+basic_specification["distance"] = [1, 2, 3]
+basic_names["distance"] = ['distance_NSW',
+                           'distance_VIC',
+                           'distance_QLD']
+
+basic_specification["male"] = [[1, 2, 3]]
+basic_names["male"] = ['male']
+
+basic_specification["agegroup"] = [[1, 2, 3]]
+basic_names["agegroup"] = ["agegroup"]
+
+basic_specification["last_choice"] = [[1, 2, 3]]
+basic_names["last_choice"] = ['last_choice']
+
+# basic_specification["seat_configuration"] = [2]
+# basic_names["seat_configuration"] = ['Airline Seat Configuration, base=No (Swissmetro)']
+#
+# basic_specification["train_survey"] = [[1, 2]]
+# basic_names["train_survey"] = ["Surveyed on a Train, base=No, (Train and Swissmetro)"]
+#
+# basic_specification["regular_class"] = [1]
+# basic_names["regular_class"] = ["First Class == False, (Swissmetro)"]
+#
+# basic_specification["single_luggage_piece"] = [3]
+# basic_names["single_luggage_piece"] = ["Number of Luggage Pieces == 1, (Car)"]
+#
+# basic_specification["multiple_luggage_pieces"] = [3]
+# basic_names["multiple_luggage_pieces"] = ["Number of Luggage Pieces > 1, (Car)"]
+
+# print basic_names
+# print basic_specification
+
+
+destination_mnl = pl.create_choice_model(data=long_testing_data,
+                                         alt_id_col=custom_alt_id,
+                                         obs_id_col=obs_id_column,
+                                         choice_col=choice_column,
+                                         specification=basic_specification,
+                                         model_type="MNL",
+                                         names=basic_names)
+
+destination_mnl.fit_mle(np.zeros(8))
+print(destination_mnl.get_statsmodels_summary())
+
+
+all_situation_ids = np.sort(long_testing_data["choice_situation"].unique())
+prediction_ids = all_situation_ids[:2000]
+prediction_df = long_testing_data.loc[long_testing_data["choice_situation"].isin(prediction_ids)].copy()
+# print(prediction_df)
+# This is the array of the predicted choice
+prediction_array = destination_mnl.predict(prediction_df)
+print(prediction_array)
+# chosen_predictions = destination_mnl.predict(prediction_df,
+#                                        choice_col=choice_column,
+#                                        return_long_probs=False)
+
+# log_likelihood_calc_1 = np.log(chosen_predictions).sum()
+# print(chosen_predictions, log_likelihood_calc_1)
+# log_predictions = np.log(prediction_array)
+# choice_array = prediction_df[choice_column].values
+# log_likelihood_calc_2 = choice_array.dot(log_predictions)
+
+# assert np.allclose(log_likelihood_calc_1,
+#                    log_likelihood_calc_2)
--- SequenceMNL/r/run.r
+++ SequenceMNL/r/run.r
@@ -5,16 +5,16 @@ new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"
 if(length(new_packages) > 0) {
   install.packages(new_packages, repos="http://cran.rstudio.com/")
 }
-library("nnet", "mlogit", "afex", "car")
+library("nnet", "mlogit")
 Data <<- read.table("/Users/daddyspro/Desktop/master_project/Data/results/sequence.csv", header = T, sep=",")
 
 # This is the function to use in R code. for sequence MNL we might user a logit 
 mu <- multinom(destination ~ HOUSINC + last_visited + GENDER + HOMESLA + HOUSEHOLD + AGEGROUP + LIFECYCLE + OVER15, data = Data)
 summary <- summary(mu)
 print(summary)
-# z <- summary(mu)$coefficients/summary(mu)$standard.errors
-# p <- (1 - pnorm(abs(z), 0,1)) * 2
-# print(p)
+z <- summary(mu)$coefficients/summary(mu)$standard.errors
+p <- (1 - pnorm(abs(z), 0,1)) * 2
+print(p)
 
 # set_sum_contrasts()
 # anova(mu, type="III")
