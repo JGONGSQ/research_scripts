78e196a41040d103592d6291f2fbde7c367ff40e
James <james.gong927@gmail.com>
2017-04-18 09:55:30 +1000

    Data prepation part done

--- ConditionalLogit/extensions.py
+++ ConditionalLogit/extensions.py
@@ -14,6 +14,8 @@ class Data(object):
     state_code = STATE_CODES
     state_list = STATE_LIST
     state_alternatives = STATE_ALTERNATIVES
+    alternative_category = ALTERNATIVE_CATEGORY
+    output_title = compulsory_fields + alternative_category + utility_variables
     counter = 0
 
     def __init__(self, source_file, output_file, forecast_file, data=None):
@@ -62,11 +64,105 @@ class Data(object):
         # print index
         return index
 
+    def _find_index_in_list(self, list, value):
+        index = None
+        for i, sub_list in enumerate(list):
+            for item in sub_list:
+                if item == value:
+                    index = i
+        return index
+
+    def _get_variable_codes(self, variable):
+        code = None
+        if variable == 'ORIGIN':
+            code = ORIGIN_CODE
+
+        elif variable == 'PARENT':
+            code = PARENT_CODE
+
+        elif variable == "GENDER":
+            code = GENDER_CODE
+
+        elif variable == 'MARITAL':
+            code = MARITAL_CODE
+
+        elif variable == 'EMPLOYMENT':
+            code = EMPLOYMENT_CODE
+
+        elif variable == 'HOUSINC':
+            code = HOUSINC_CODE
+
+        elif variable == 'LIFECYCLE':
+            code = LIFECYCLE_CODE
+
+        elif variable == 'AGEGROUP':
+            code = AGEGROUP_CODE
+
+        return code
+
+    def _get_variable_list(self, variable):
+        list = None
+        if variable == 'ORIGIN':
+            list = ORIGIN_LIST
+
+        elif variable == 'PARENT':
+            list = PARENT_LIST
+
+        elif variable == "GENDER":
+            list = GENDER_LIST
+
+        elif variable == 'MARITAL':
+            list = MARITAL_LIST
+
+        elif variable == 'EMPLOYMENT':
+            list = EMPLOYMENT_LIST
+
+        elif variable == 'HOUSINC':
+            list = HOUSINC_LIST
+
+        elif variable == 'LIFECYCLE':
+            list = LIFECYCLE_LIST
+
+        elif variable == 'AGEGROUP':
+            list = AGEGROUP_LIST
+
+        return list
+
+    def _get_variable_data(self, variable_code, variable, utility_data):
+        # user the variable get variable value in thr row
+        variable_value = utility_data.__getitem__(self.utility_variables.index(variable))
+        if variable == 'ORIGIN':
+            variable_value = variable_value[0]
+        # I know the variable code, using the value to find the index value of the value in the code
+        index = self._find_index_in_list(variable_code, variable_value)
+        # use the index value to get the choose category
+        data = self._get_variable_list(variable).__getitem__(index)
+
+        return data
+
+    def _convert_utility_data(self, utility_data):
+        converted_data = list()
+        # print("This is the utility data", utility_data)
+
+        # For each vaiable in the utility variable list
+        for variable in self.utility_variables:
+            variable_code = self._get_variable_codes(variable)
+            if variable_code:
+                variable_data = self._get_variable_data(variable_code, variable, utility_data)
+                converted_data.append(variable_data)
+            else:
+                converted_data.append(utility_data.__getitem__(int(self.utility_variables.index(variable))))
+        # print("This is the converted data", converted_data)
+        return converted_data
+
     def _get_useful_data(self, title_row, row, utility_data):
         data = list()
 
         # get the number of trips in the visit and return the number of the visited
         number_of_trips, alternatives = self._get_trips_and_alternatives(title_row, row)
+        tourist_id = row.__getitem__(title_row.index('TOURIST_ID'))
+
+        converted_utility_data = self._convert_utility_data(utility_data)
 
         # then process each of them into a list
         if number_of_trips > 1:
@@ -74,7 +170,7 @@ class Data(object):
             # print number_of_trips, alternatives
             sort_list = self._bubble_sort_the_alternatives(alternatives)
 
-            print(number_of_trips, alternatives, sort_list)
+            # print(number_of_trips, alternatives, sort_list)
             # for i in range(number_of_trips):
             for alternative in alternatives:
                 self.counter += 1
@@ -85,11 +181,12 @@ class Data(object):
                     else:
                         choice_flag = "0"
 
-                    row = [self.counter, choice, choice_flag] + utility_data
+                    row = [tourist_id, self.counter, choice, choice_flag] + converted_utility_data
 
+                    data.append(row)
                     print(row)
 
-            raise Exception
+            # raise Exception
 
         return data
 
@@ -108,6 +205,8 @@ class Data(object):
         self.read(self.source_file)
         title_row = None
         data = list()
+
+        data.append(self.output_title)
 
         for i, row in enumerate(self.data):
             # print("### Start of the line ###")
--- ConditionalLogit/settings.py
+++ ConditionalLogit/settings.py
@@ -16,8 +16,37 @@ UTILITY_VARIABLES = [
     'OVER15'
 ]
 
-COMPULSORY_FIELDS = ['id', 'TOURIST_ID']
+COMPULSORY_FIELDS = ['TOURIST_ID']
+ALTERNATIVE_CATEGORY = ['choice_situation', 'alternative_id', 'choice']
 
 STATE_LIST = ['NSW', 'VIC', 'QLD', 'SA', 'TAS', 'NT']
 STATE_CODES = ['1', '2', '3', '4', '6', '8']
 STATE_ALTERNATIVES = ['1', '2', '3', '4', '5', '6']
+
+
+ORIGIN_LIST = ['NSW', 'VIC', 'QLD', 'SA', 'WA', 'TAS', 'NT', 'ACT']
+ORIGIN_CODE = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '8'], ['9', '10'], ['11'], ['12'], ['13']]
+
+PARENT_LIST = ['YES', 'NO', 'DONT_KNOW']
+PARENT_CODE = [['1'], ['0', '2'], ['9']]
+
+CH15TO24_LIST = ['YES', 'NO', 'DONT_KNOW']
+CH15TO24_CODE = [['1'], ['0', '2'], ['9']]
+
+GENDER_LIST = ['MALE', 'FEMALE']
+GENDER_CODE = [['1'], ['2']]
+
+MARITAL_LIST = ['SINGLE', 'COUPLE', 'REFUSED']
+MARITAL_CODE = [['1'], ['2'], ['9']]
+
+EMPLOYMENT_LIST = ['WORKING', 'NOT_WORKING', 'RETIRED', 'STUDYING', 'DONT_KNOW']
+EMPLOYMENT_CODE = [['1', '2', '5'], ['3'], ['4'], ['6'], ['7', '8', '9']]
+
+HOUSINC_LIST = ['LOW', 'MEDIUM', 'HIGH', 'DONT_KONW']
+HOUSINC_CODE = [['1', '2', '3', '4', '5'], ['6', '7', '8', '9'], ['11', '12'], ['10', '13', '99', '25', '28', '29']]
+
+LIFECYCLE_LIST = ['SINGLE', 'COUPLE_NO_KIDS', 'COUPLE_WITH_KIDS', 'DONT_KNOW']
+LIFECYCLE_CODE = [['1', '2', '3', '8', '9'], ['4', '10', '11'], ['5', '6', '7'], ['0']]
+
+AGEGROUP_LIST = ['15_29', '30_39', '40_49', '50_59', '60_69', '70+']
+AGEGROUP_CODE = [['1', '2', '3'], ['4', '5'], ['6', '7'], ['8', '9'], ['10', '11'], ['12']]
